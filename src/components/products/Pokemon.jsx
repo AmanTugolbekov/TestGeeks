b8aec9aac771d8be63435905bb534f8e7b0a2ede
AmanTugolbekov <161336104+AmanTugolbekov@users.noreply.github.com>
2024-06-29 19:12:34 +0600

    Update Pokemon.jsx

--- src/components/products/Pokemon.jsx
+++ src/components/products/Pokemon.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { API } from "../../helpers/const";
 import PokemonVerstka from "./PokemonVerstka";
 import axios from "axios";
-
+import nerd from "../../assets/nerd.png";
 const Pokemon = () => {
   const [pokemon, setPokemon] = useState([]);
   const APIfunc = async () => {
@@ -19,12 +19,36 @@ const Pokemon = () => {
     <div
       style={{
         display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
+        alignItems: "center",
         padding: "2rem",
-        gap: "3rem",
+        backgroundColor: "yellow",
+        flexWrap: "wrap",
+        gap: "2rem",
+        paddingLeft: "6rem",
       }}
     >
+      <header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "100%",
+          maxWidth: "80rem",
+          marginBottom: "2rem",
+          paddingBottom: "1rem",
+          borderBottom: "1px solid #D1D5DB",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+          <img
+            alt="logo"
+            src={nerd}
+            style={{ width: "2.5rem", height: "2.5rem" }}
+          />
+          <span style={{ fontSize: "2rem", fontWeight: "bold" }}>GEEKS</span>
+        </div>
+      </header>
+
       {pokemon.map((elem) => (
         <PokemonVerstka key={elem.id} pokemon={elem} />
       ))}
